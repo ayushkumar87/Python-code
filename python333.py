be47f1b9482ae54cda92866820a09a2c92062288
Ayush Kumar <152207841+ayushkumar87@users.noreply.github.com>
2023-12-14 22:54:05 +0530

    OOps Concept

---
+++ python333.py
@@ -0,0 +1,10 @@
+class Circle:
+    pi=3.14
+    def__init__(self,radius):
+        self.radius=radius
+    def get_area(self):
+        c=self.pi*self.radius*self.radius
+        return c
+circle1=Circle(4)
+print(c)
+
